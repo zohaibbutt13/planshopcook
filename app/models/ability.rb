fe510b3f02e01a1efdd43acfb8b5bfbd0d054e93
Zohaib Butt <zohaibnadeem13@gmail.com>
2021-07-01 01:58:42 +0500

    ability file updated

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -16,6 +16,9 @@ class Ability
     if user.present?
       if user.is_admin?
         can [:home], :admin
+        can [:new, :create, :index, :show, :favorite_recipes, :add_to_favorite, :remove_from_favorite], Recipe
+        can [:new], Ingredient
+        can [:new, :create], MealPlan
       else
         can [:new, :create, :index, :show, :favorite_recipes, :add_to_favorite, :remove_from_favorite], Recipe
         can [:new], Ingredient
